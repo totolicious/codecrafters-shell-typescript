5fb554e9d9f86066fe59be060abf027d4277e82b
Totolicious <totolicious@users.noreply.github.com>
2026-06-10 10:15:19 +0300

    codecrafters submit [skip ci]

--- app/commands/type.ts
+++ app/commands/type.ts
@@ -4,7 +4,7 @@ import { allCommands } from "./_allCommands";
 export const type: Command = (args: string[]) => {
   const commandName = args[0];
   if (allCommands[commandName] !== undefined) {
-    console.log(`${commandName} is a shell built-in`);
+    console.log(`${commandName} is a shell builtin`);
     return;
   }
 
