ca22272afac695ff9c5cca124fbb4b2141ac97b1
Totolicious <totolicious@users.noreply.github.com>
2026-06-12 12:27:29 +0300

    codecrafters submit [skip ci]

--- app/completer/commandCompleter.ts
+++ app/completer/commandCompleter.ts
@@ -5,19 +5,45 @@ import { ringBell } from "../repl/ringBell";
 import { getBuiltinCommandCompletion } from "./getBuiltinCommandCompletion";
 import { getPathCommandCompletion } from "./getPathCommandCompletion";
 
+// This is the correct form of the completion handler. The builtin and binary commands should be mixed to form a single array of sorted results
+// The exercise wants to first look at the builtin commands, and if none are found, look at the binary commands - which is NOT the correct behaviour
+// export const commandCompleter: Completer = async (
+//   line,
+// ): Promise<CompleterResult> => {
+//   const trimmedLine = line.trim();
+//   const builtinCommands = getBuiltinCommandCompletion(trimmedLine);
+//   const pathCommands = await getPathCommandCompletion(trimmedLine);
+
+//   const commands = [...builtinCommands, ...pathCommands].sort();
+
+//   if (!commands.length) {
+//     ringBell();
+//     return [[], line];
+//   }
+
+//   return [commands.length === 1 ? [`${commands[0]} `] : commands, line];
+// };
+
+// code adjusted to fit the exercise incorrect requirements
+
 export const commandCompleter: Completer = async (
   line,
 ): Promise<CompleterResult> => {
   const trimmedLine = line.trim();
   const builtinCommands = getBuiltinCommandCompletion(trimmedLine);
-  const pathCommands = await getPathCommandCompletion(trimmedLine);
 
-  const commands = [...builtinCommands, ...pathCommands].sort();
-
-  if (!commands.length) {
-    ringBell();
-    return [[], line];
+  if (builtinCommands.length) {
+    return [
+      builtinCommands.length === 1
+        ? [`${builtinCommands[0]} `]
+        : builtinCommands,
+      line,
+    ];
   }
 
-  return [commands.length === 1 ? [`${commands[0]} `] : commands, line];
+  const pathCommands = await getPathCommandCompletion(trimmedLine);
+  return [
+    pathCommands.length === 1 ? [`${pathCommands[0]} `] : pathCommands,
+    line,
+  ];
 };
